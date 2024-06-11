7fadf39809fdf9ef3d15c105b5a89a324f7a05a0
ryohei-jimbo-mjs <69183505+jimbo337@users.noreply.github.com>
2024-06-11 05:26:50 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,21 +1,36 @@
 // Create Web Server
-// Run the server
-// Test the server
+// Create a web server that listens on port 3000 and serves the comments.html file
+// Create a route that serves the comments.json file when the URL path is /api/comments
+// Use the fs module to read the comments.json file and send the contents back to the client
 
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const app = express();
-
-// app.use(express.static('./public'));
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const server = http.createServer((req, res) => {
+    if (req.url === '/api/comments') {
+        fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end();
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(data);
+            }
+        });
+    } else {
+        fs.readFile(path.join(__dirname, 'comments.html'), 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end();
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
 });
 
-app.get('/api/comments', (req, res) => {
-    res.send('Comments');
-});
-
-app.listen(3000, () => {
-    console.log('Server is running');
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
